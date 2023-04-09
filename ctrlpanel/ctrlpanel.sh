9b70ad4bf6ba264f5b02e6a800a7e31c28a9f92c
Moira Campbell <wowitsvivian@gmail.com>
2023-04-09 16:40:39 +0200

    fix: opens fish, updated readme

--- ctrlpanel/ctrlpanel.sh
+++ ctrlpanel/ctrlpanel.sh
@@ -1,1 +1,1 @@
-tmux new-session 'exec btop' \; \split-window -h 'exec ctrlcal.sh'\; \split-window -v 'exec ncdu'\; \split-window -v \; \select-pane -t 0
+tmux new-session 'exec btop' \; \split-window -h 'exec ctrlcal.sh'\; \split-window -v 'exec ncdu'\; \split-window -v 'exec fish'
